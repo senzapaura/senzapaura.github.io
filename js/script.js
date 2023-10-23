905e878fd325621272ea4a8371a1968cbb4c49fa
Khalil Hm <khalilhamdouch60@gmail.com>
2023-10-23 17:08:01 +0200

    added
    added

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.getElementById('worksPage').addEventListener('click', function() {
+    window.location.href = "works.html";
+});
+
+document.getElementById('resumePage').addEventListener('click', function() {
+    window.location.href = "resume.html";
+});
+
+document.getElementById('servicesPage').addEventListener('click', function() {
+    window.location.href = "services.html";
+});
+
+document.getElementById('contactPage').addEventListener('click', function() {
+    window.location.href = "contact.html";
+});
