bc6d45a212c21f3ef67c8fe46b63d94d58e1ce09
Khalil Hm <khalilhamdouch60@gmail.com>
2023-10-24 01:22:10 +0200

    final vers 24oct

--- js/script.js
+++ js/script.js
@@ -13,9 +13,20 @@ document.getElementById("servicesPage").addEventListener("click", function () {
 document.getElementById("contactPage").addEventListener("click", function () {
   window.location.href = "Contact.html";
 });
-
 function changeImage(src) {
-  let mainImage = document.getElementById("mainImage");
+  let clickedThumbnail = event.currentTarget;
+  let thumbnailsContainer = clickedThumbnail.parentElement;
+  let workContainer = thumbnailsContainer.parentElement;
+  let mainImage = workContainer.querySelector(".mainImage");
+
+  // Remove active class from all thumbnails in the same container
+  thumbnailsContainer.querySelectorAll(".thumbnail").forEach(function (thumb) {
+    thumb.classList.remove("active");
+  });
+
+  // Add active class to the clicked thumbnail
+  clickedThumbnail.classList.add("active");
+
   mainImage.src = src;
   mainImage.style.transition = "opacity 0.4s";
   mainImage.style.opacity = 0;
@@ -24,3 +35,37 @@ function changeImage(src) {
     mainImage.style.opacity = 1;
   }, 300);
 }
+
+function showPopup(popupId) {
+  document.getElementById(popupId).style.display = "block";
+}
+
+function hidePopup(popupId) {
+  document.getElementById(popupId).style.display = "none";
+}
+
+function copyLink() {
+  var copyText = document.querySelector("#sharePopup input");
+  copyText.select();
+  document.execCommand("copy");
+}
+
+document.addEventListener("scroll", function () {
+  var element = document.querySelector(".sticky-buttons"); // your sticky element
+  var container = document.querySelector(".work1class"); // parent container
+
+  var containerBounds = container.getBoundingClientRect();
+
+  if (
+    window.scrollY >= containerBounds.top &&
+    window.scrollY <= containerBounds.bottom
+  ) {
+    // Within the container bounds, make it sticky
+    element.style.position = "fixed";
+    element.style.top = "60px";
+    element.style.right = "42px"; // or any offset you prefer
+  } else {
+    // Outside of container bounds, revert to default position
+    element.style.position = "absolut";
+  }
+});
