a712b8bdce8f7bab09a2dfed9adc64ac55108123
Khalil Hm <khalilhamdouch60@gmail.com>
2023-10-23 17:53:35 +0200

    Zdna

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
-document.getElementById('worksPage').addEventListener('click', function() {
-    window.location.href = "works.html";
+document.getElementById("worksPage").addEventListener("click", function () {
+  window.location.href = "Works.html";
 });
 
-document.getElementById('resumePage').addEventListener('click', function() {
-    window.location.href = "resume.html";
+document.getElementById("resumePage").addEventListener("click", function () {
+  window.location.href = "Resume.html";
 });
 
-document.getElementById('servicesPage').addEventListener('click', function() {
-    window.location.href = "services.html";
+document.getElementById("servicesPage").addEventListener("click", function () {
+  window.location.href = "Services.html";
 });
 
-document.getElementById('contactPage').addEventListener('click', function() {
-    window.location.href = "contact.html";
+document.getElementById("contactPage").addEventListener("click", function () {
+  window.location.href = "Contact.html";
 });
+
+function changeImage(src) {
+  let mainImage = document.getElementById("mainImage");
+  mainImage.src = src;
+  mainImage.style.transition = "opacity 0.3s";
+  mainImage.style.opacity = 0;
+
+  setTimeout(() => {
+    mainImage.style.opacity = 1;
+  }, 300);
+}
